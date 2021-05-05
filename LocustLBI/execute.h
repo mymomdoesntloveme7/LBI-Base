65bda5077e71a34baded4f59fdcf41d271d537d7
deadlocust <70506265+deaddlocust@users.noreply.github.com>
2021-05-05 12:27:29 +0800

    Update execute.h

--- LocustLBI/execute.h
+++ LocustLBI/execute.h
@@ -20,12 +20,13 @@ void InitLBI()
 	r_spawn(rL);
 }
 
-void Execute(int rL, std::string c) 
+void Execute(int rL, std::string script) 
 {
+	replaceAll(script, "game:HttpGet", "HttpGet");
 	lua_State* VM = lua_open();
 	luaL_openlibs(VM);
 
-	if (luaL_loadbuffer(VM, c.c_str(), c.size(), "LBI")) {
+	if (luaL_loadbuffer(VM, script.c_str(), script.size(), "LBI")) {
 		const char* error = lua_tostring(L, -1);
 		r_getfield(rL, -10002, "warn");
 		r_pushlstring(rL, error, strlen(error));
